d0662a2fcb50cb8e21ca6c3c001d6754565ee001
Brandon Bush <brandon.bush@rsdcgroup.com>
2018-11-24 11:13:47 -0500

    Prepped for Demo

--- src/testimport.py
+++ src/testimport.py
@@ -2,10 +2,12 @@ import testruns
 
 print('In the import')
 
+#test whether we can access variables in the other file
 g = testruns.l
 
 print(g)
 
+#Return a list of odd numbers between inputs l and r
 def oddNumbers(l, r):
     retval = []
     for i in range(l, (r+1)):
--- src/testruns.py
+++ src/testruns.py
@@ -1,28 +1,34 @@
 import debug_dec
 
-g = dict(song = 'gangam style', run = '10k', home = 'NYC')
-
-for k, v in g.items():
-    print('This {0} is {1}'.format(k, v))
-
-#print('Trying it {}.{}.{}'.format(**g))
-
+#Use the debug decorator function
 @debug_dec.debug
 def foo(song, home, run):
     print(song + ' ' + home + ' ' + run)
 
-foo(**g) 
+def main(**kwargs):
+    print('This is the main function')
 
+#Only runs if this script is the one invoked from the command line
+if __name__ == '__main__':
+    main()
+
+#Playing with dictionaries
+testvar = dict(song = 'gangam style', run = '10k', home = 'NYC')
+
+for k, v in testvar.items():
+    print('This {0} is {1}'.format(k, v))
+
+foo(**testvar) 
+
+#Easily break apart a list
 l = [1, 2, 3, 4, 5]
 
 first, *rest, last = l
 
 print(str(first) + ' cool ' + str(rest) + ' ' + str(last))
 
-def main(**kwargs):
-    print('This is the main function')
+#Playing with lambdas and list comprehension
+squares = lambda x: x*x
+testlist = [squares(x) for x in range(10)]
 
-if __name__ == '__main__':
-    main()
-
-
+print(', '.join(str(t) for t in testlist))
